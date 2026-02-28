a3ad85eb390894d316780bb5e985b29fb5987852
Alan Ayala <aar80810@lasallebajio.edu.mx>
2026-02-27 23:18:20 -0600

    Ejercicio 5 terminado

---
+++ app/lib/src/main/java/TareaPoo/animales.kt
@@ -0,0 +1,42 @@
+package TareaPoo
+
+open class Animal(val nombre: String, val edad: Int) {
+    open fun hacerSonido() {
+        println("Sonido genérico.")
+    }
+    fun describirse() {
+        println("Soy $nombre y tengo $edad años.")
+    }
+}
+
+class Perro(nombre: String, edad: Int) : Animal(nombre, edad) {
+    override fun hacerSonido() {
+        println("¡Guau!")
+    }
+}
+
+class Gato(nombre: String, edad: Int) : Animal(nombre, edad) {
+    override fun hacerSonido() {
+        println("¡Miau!")
+    }
+}
+
+class Vaca(nombre: String, edad: Int) : Animal(nombre, edad) {
+    override fun hacerSonido() {
+        println("¡Mu!")
+    }
+}
+
+fun main() {
+    val perro = Perro("Rex", 3)
+    perro.describirse()
+    perro.hacerSonido()
+
+    val gato = Gato("Michi", 2)
+    gato.describirse()
+    gato.hacerSonido()
+
+    val vaca = Vaca("Lola", 5)
+    vaca.describirse()
+    vaca.hacerSonido()
+}
